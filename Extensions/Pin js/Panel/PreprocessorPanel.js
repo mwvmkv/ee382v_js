b8db8fb0e3840264820d6d28d2cfaca255dbd244
mwvmkv <mwv262@gmail.com>
2014-11-28 20:23:24 -0600

    Fixed example with replacing user code in the preprocessor

--- Extensions/Pin js/Panel/PreprocessorPanel.js
+++ Extensions/Pin js/Panel/PreprocessorPanel.js
@@ -13,6 +13,9 @@ function preprocessor(source, url, listenerName) {
 
     // End Minified Code
 
+   var preprocessedSource = source;
+   var functionNames = [];
+
  //<insert user code here> 
   url = url ? url : '(eval)';
   url += listenerName ? '_' + listenerName : '';
@@ -51,12 +54,16 @@ function extractPreprocessedFiles(onExtracted) {
 function reloadWithPreprocessor(injectedScript) {
   
 var userCode = '\r\n    function traverse(node, func) {\r\n        \r\n    if(node.hasOwnProperty(\"type\")){func(node);}\r\n    \r\n    for (var key in node) { \/\/2\r\n        if (node.hasOwnProperty(key)) { \/\/3\r\n            var child = node[key];\r\n            if (typeof child === \'object\' && child !== null) { \/\/4\r\n\r\n                if (Array.isArray(child)) {\r\n                    child.forEach(function(node) { \/\/5\r\n                        traverse(node, func);\r\n                    });\r\n                } else {\r\n                    traverse(child, func); \/\/6\r\n                }\r\n            }\r\n        }\r\n    }\r\n}\r\n       var functionNames = []; \r\n       var instrumentationCode = \"window.__Pin_JS_InstrumentationResults[\'<rep>\']++;\";\r\n        function visitor(node){\r\n        if(node.type == \"FunctionDeclaration\")\r\n            {\r\n            functionNames.push(node.id.name);\r\n            var insertCode = esprima.parse(instrumentationCode.replace(\/\\<rep\\>\/g, node.id.name));\r\n            node.body.body = insertCode.body.concat(node.body.body);\r\n            }\r\n        }\r\n        \r\n       var tree = esprima.parse(source, { tolerant: true, loc: true, range: true });\r\n       traverse(tree, visitor);\r\n       var preprocessedSource = escodegen.generate(tree);\r\n    ';
+var preprocessorString =  '(' + preprocessor + ')';
+preprocessorString = preprocessorString.replace(/\/\/\<insert user code here\>/g, userCode)
   var options = {
     ignoreCache: true,
     userAgent: undefined,
     injectedScript: '(' + injectedScript  + ')()',
-    preprocessingScript: '(' + JSON.strinify(preprocessor).replace(/\/\/\<insert user code here\>/, userCode) + ')'
-  };
+    preprocessingScript: preprocessorString
+};
+
+
   chrome.devtools.inspectedWindow.reload(options);
 }
 
